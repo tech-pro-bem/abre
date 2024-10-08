93b6043954ebbf29a0da709cfccfb2b0cbea9437
Kalini Veloso <kaliniveloso41@gmail.com>
2024-10-08 18:30:14 -0300

    refactor: changes in style

--- src/app/components/about.tsx
+++ src/app/components/about.tsx
@@ -8,8 +8,7 @@ interface SobreNos {
 }
 const sobreNos: SobreNos = {
   title: "Conheça a ABRE",
-  text: `
-  Somos a ABRE e nossa missão é acolher pessoas com esquizofrenia e de seus familiares. 
+  text: `Somos a ABRE e nossa missão é acolher pessoas com esquizofrenia e de seus familiares. 
   Em nossos projetos, conseguimos trazer as forças e potencialidades de cada pessoa, promovendo o bem-estar na comunidade.
   Acreditamos que as pessoas são mais que um diagnóstico. Por isso, compartilhamos conhecimento sempre com ética e transparência.
   Nossa ONG é formada por pessoas que lutam para garantir os direitos, reduzir o estigma e promover diálogos sobre a doença.
@@ -28,7 +27,7 @@ const stylizeWords = (text: string) => {
   return text
     .split(regex)
     .map((words, index) =>
-      wordsInBold.includes(words) ? <strong key={index}>{words}</strong> : words
+      wordsInBold.includes(words) ? <span key={index}>{words}</span> : words
     );
 };
 
@@ -38,24 +37,29 @@ export const About = () => {
       <div className="container__about__buttons">
         <button className="container__about__button">Conheça a ABRE</button>
       </div>
+      <div className="container__about__title__text__video">
+        <div className="container__about__title__text">
+          <h1 className="container__about__title">{sobreNos.title}</h1>
 
-      <h2 className="container__about__title">{sobreNos.title}</h2>
-      <div className="container__about__text">
-        {sobreNos.text.split("\n").map((paragraph, index) => (
-          <p
-            className="container__about__text__paragraph"
-            key={index}
-          >
-            {stylizeWords(paragraph)}
-          </p>
-        ))}
+          <div className="container__about__text">
+            {sobreNos.text.split("\n").map((paragraph, index) => (
+              <p
+                className="container__about__text__paragraph"
+                key={index}
+              >
+                {stylizeWords(paragraph)}
+              </p>
+            ))}
+          </div>
+        </div>
+        <iframe
+          className="container__about__video"
+          src={sobreNos.video}
+          title="Escutem a Nossa Voz"
+          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+          allowFullScreen
+        ></iframe>
       </div>
-      <iframe
-        className="container__about__video"
-        src={sobreNos.video}
-        title="Escutem a Nossa Voz"
-        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-      ></iframe>
     </div>
   );
 };
