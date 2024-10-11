35db9e95ae5a32ff1e985883985be09cd60a63b2
Pedro Menezes <pdrmenezes1@gmail.com>
2024-10-10 23:54:25 -0300

    styles: cleanup and improve classnames

--- src/components/projects-section/projects-icon.tsx
+++ src/components/projects-section/projects-icon.tsx
@@ -1,26 +1,23 @@
 export function ProjectsIcon({
-  children,
   component: Component,
   isActive,
   text,
 }: {
-  children?: React.ReactNode;
   component?: React.ReactNode;
   isActive: boolean;
   text?: string;
 }) {
   return (
-    <div className="project-icon">
+    <div className="project-icon-container">
       <div
-        className="project-circle"
+        className="project-icon-circle"
         style={{
           backgroundColor: isActive ? "var(--color-primary-dark)" : "var(--color-primary-lightest)",
         }}
       >
-        {children ? children : null}
         {Component ? Component : null}
       </div>
-      {text && <span className="project-text">{text}</span>}
+      {text && <span className="project-title">{text}</span>}
     </div>
   );
 }
