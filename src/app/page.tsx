038ed1a75d49b9c9b4357a8e9603fef64142bbab
Pedro Menezes <pdrmenezes1@gmail.com>
2024-10-10 23:52:56 -0300

    feat: add framer-motion lib

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,8 +2,8 @@ import { ProjectsSection } from "@/components/projects-section";
 
 export default function Home() {
   return (
-    <div>
+    <>
       <ProjectsSection />
-    </div>
+    </>
   );
 }
