715b697f7536c6977fb4824437525d653495e74c
Pedro Menezes <pdrmenezes1@gmail.com>
2024-10-16 18:54:10 -0300

    feat: improve carousel semantic tags + create Props type + improve animation

--- src/components/projects-carousel/index.tsx
+++ src/components/projects-carousel/index.tsx
@@ -1,38 +1,42 @@
-import "./style.css";
-import { motion } from "framer-motion";
+import styles from "./styles.module.css";
+import { AnimatePresence, motion, MotionConfig } from "framer-motion";
 import { mock_projects_data } from "@/mock-data/projects";
 import { FaChevronLeft, FaChevronRight } from "react-icons/fa6";
 import { Slide } from "./slide/slide";
+
+type ProjectsCarouselProps = {
+  activeProjectIndex: number;
+  direction: "right" | "left" | null;
+  handlePreviousSlide: () => void;
+  handleNextSlide: () => void;
+};
 
 export const ProjectsCarousel = ({
   activeProjectIndex,
   direction,
   handlePreviousSlide,
   handleNextSlide,
-}: {
-  activeProjectIndex: number;
-  direction: "right" | "left" | null;
-  handlePreviousSlide: () => void;
-  handleNextSlide: () => void;
-}) => {
+}: ProjectsCarouselProps) => {
+  const motionInitial = direction === "right" ? "hiddenRight" : "hiddenLeft";
   const slideVariants = {
     hiddenRight: {
-      x: "50%",
+      x: "100%",
       opacity: 0,
     },
     hiddenLeft: {
-      x: "-50%",
+      x: "100%",
       opacity: 0,
     },
     visible: {
       x: "0",
       opacity: 1,
       transition: {
-        duration: 1,
+        duration: 0.8,
       },
     },
     exit: {
       opacity: 0,
+      x: direction === "right" ? "-100%" : "100%",
       transition: {
         duration: 0.5,
       },
@@ -40,36 +44,44 @@ export const ProjectsCarousel = ({
   };
 
   return (
-    <div className="carousel">
-      <div className="carousel-images">
-        <motion.div
-          key={activeProjectIndex}
-          initial={direction === "right" ? "hiddenRight" : "hiddenLeft"}
-          animate="visible"
-          exit="exit"
-          variants={slideVariants}
-          style={{ padding: "0.2rem" }}
-        >
-          <Slide
-            title={mock_projects_data[activeProjectIndex].title}
-            content={mock_projects_data[activeProjectIndex].description}
-            icon={mock_projects_data[activeProjectIndex].icon}
-          />
-        </motion.div>
-        <div className="slide_direction">
-          <div
-            className="left"
+    <div
+      role="region"
+      aria-label="Carousel"
+      className={styles.carousel}
+    >
+      <div className={styles.carousel_images}>
+        <MotionConfig reducedMotion="user">
+          <AnimatePresence mode="wait">
+            <motion.ul
+              key={activeProjectIndex}
+              initial={motionInitial}
+              animate="visible"
+              exit="exit"
+              variants={slideVariants}
+              className={styles.carousel_images_container}
+            >
+              <Slide
+                title={mock_projects_data[activeProjectIndex].title}
+                content={mock_projects_data[activeProjectIndex].description}
+                icon={mock_projects_data[activeProjectIndex].icon}
+              />
+            </motion.ul>
+          </AnimatePresence>
+        </MotionConfig>
+        <nav className={styles.carousel_nav}>
+          <button
+            className={styles.left}
             onClick={handlePreviousSlide}
           >
             <FaChevronLeft size={26} />
-          </div>
-          <div
-            className="right"
+          </button>
+          <button
+            className={styles.right}
             onClick={handleNextSlide}
           >
             <FaChevronRight size={26} />
-          </div>
-        </div>
+          </button>
+        </nav>
       </div>
     </div>
   );
