dc520d7872cae83350dd1dc884e13fc8320855bd
Pedro Menezes <pdrmenezes1@gmail.com>
2024-10-23 12:58:18 -0300

    feat: create & add contact section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
+import { ContactSection } from "@/components/contact-section";
 import { ProjectsSection } from "@/components/projects-section";
 
 export default function Home() {
   return (
     <>
       <ProjectsSection />
+      <ContactSection />
     </>
   );
 }
---
+++ src/components/contact-section/index.tsx
@@ -0,0 +1,39 @@
+import { FacebookIcon, InstagramIcon } from "../icons";
+import styles from "./styles.module.css";
+
+export async function ContactSection() {
+  return (
+    <section className={styles.section}>
+      <div className={styles.content_wrapper}>
+        <h2 className={`${styles.projects_section_title} section-title`}>Converse com a gente</h2>
+        <p className={styles.projects_paragraph}>
+          Se você busca acolhimento para você ou para outra pessoa, envie uma mensagem para gente.
+          Estamos presentes no Facebook e Instagram para tirar dúvidas e aconselhar pessoas com
+          esquizofrenia ou familiares e amigos.
+        </p>
+        <div className={styles.buttons_container}>
+          <a
+            href="https://www.facebook.com/abre.esquizofrenia/"
+            target="_blank"
+            rel="noopener noreferrer"
+            className={styles.anchor}
+          >
+            <button className={styles.button}>
+              <FacebookIcon /> Conversar via Facebook
+            </button>
+          </a>
+          <a
+            href="https://www.instagram.com/abreesquizofrenia/"
+            target="_blank"
+            rel="noopener noreferrer"
+            className={styles.anchor}
+          >
+            <button className={styles.button}>
+              <InstagramIcon /> Conversar via Instagram
+            </button>
+          </a>
+        </div>
+      </div>
+    </section>
+  );
+}
