656c17e975ab6d5b19635ec091b34008e14b7688
Kalini Veloso <kaliniveloso41@gmail.com>
2024-10-25 21:13:00 -0300

    feat: materials section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
+import { Materials } from "@/components/materials/materials";
 import { ProjectsSection } from "@/components/projects-section";
 
 export default function Home() {
   return (
     <>
       <ProjectsSection />
+      <Materials />
     </>
   );
 }
---
+++ src/components/materials/materials.tsx
@@ -0,0 +1,51 @@
+"use client";
+import { section } from "framer-motion/client";
+import styles from "./styles.module.css";
+import Image from "next/image";
+import mandala from "../../../public/mandala.png";
+
+interface Material {
+  title: string;
+  text: string;
+}
+const material: Material = {
+  title: "Nossos materiais",
+  text: `Temos diversos documentos para informar sobre a esquizofrenia e vencer o estima.
+  Confira uma página com relatórios de atividades da ONG, livros, livretos, manuais e guias para você saber mais sobre o diagnóstico.`,
+};
+
+export const Materials = () => {
+  return (
+    <section>
+      <div className={styles.content}>
+        <div className={styles.title_text}>
+          <h1 className={styles.title}>{material.title}</h1>
+          <div className={styles.text}>
+            {material.text.split("\n").map((paragraph, index) => (
+              <p
+                className={styles.paragraph}
+                key={index}
+              >
+                {" "}
+                {paragraph}
+              </p>
+            ))}
+          </div>
+          <button className={styles.button_materials}>Acessar materiais</button>
+        </div>
+        <div className={styles.container_image}>
+          <Image
+            className={styles.image_mandala}
+            src={mandala}
+            alt="Picture of the author"
+            sizes="100vw"
+            style={{
+              width: "100%",
+              height: "auto",
+            }}
+          />
+        </div>
+      </div>
+    </section>
+  );
+};
