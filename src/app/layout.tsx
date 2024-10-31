32438ec9d4c7101bd9da99669b697118bc8a3740
JaySant <jaysant15@gmail.com>
2024-10-31 02:03:33 -0300

    feat: add hero component for desktop and mobile

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,8 @@ import type { Metadata } from "next";
 import { Jost } from "next/font/google";
 import "./styles/reset.css";
 import "./styles/globals.css";
+import "./styles/theme.css";
+import { Hero } from "./hero";
 
 const jost = Jost({
   weight: ["400", "500", "600", "700"],
@@ -22,7 +24,10 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={jost.className}>{children}</body>
+      <body className={jost.className}>
+        <Hero />
+        {children}
+      </body>
     </html>
   );
 }
