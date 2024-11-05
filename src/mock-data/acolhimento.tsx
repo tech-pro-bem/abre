c12976530c17b7272a75d15818d047fb851c3447
Pedro Menezes <pdrmenezes1@gmail.com>
2024-11-04 23:40:55 -0300

    chore: add temp mock data for 'acolhimento' section

---
+++ src/mock-data/acolhimento.tsx
@@ -0,0 +1,23 @@
+export const mock_acolhimento_data = [
+  {
+    title: "Lista de Instituições",
+    description:
+      "Confira uma lista de instituições que discutem esquizofrenia e outros temas ligados ao transtorno. Ao escolher uma instituição, iremos te redirecionar para uma página externa para você pesquisar mais sobre o assunto.",
+    button_text: "Conferir instituições",
+    button_href: "",
+  },
+  {
+    title: "Publicações do IPUB",
+    description:
+      "O Instituto de Psiquiatria da Universidade Federal do Rio de Janeiro (UFRJ) reúne diversas publicações sobre atenção psicossocial e que dialogam com outras áreas de conhecimento.",
+    button_text: "Conferir publicações",
+    button_href: "https://www.ipub.ufrj.br/arquivos-do-ipub-online/",
+  },
+  {
+    title: "Lista de CAPS",
+    description:
+      "Centros de Atenção Psicossocial (CAPS) são lugares onde a comnunidade consegue atendimento focado em saúde mental, seja para situações difíceis ou reabilitação psicossocial.",
+    button_text: "Conferir CAPS",
+    button_href: "",
+  },
+];
