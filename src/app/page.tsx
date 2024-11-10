ab2ace30e16a4bb9c344a7d2fbdaf034f11fe204
Kalini Veloso <kaliniveloso41@gmail.com>
2024-11-09 21:44:03 -0300

    feat: creation of the faq section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
+import { Faq } from "@/components/faq/faq";
 import { ProjectsSection } from "@/components/projects-section";
 
 export default function Home() {
   return (
     <>
       <ProjectsSection />
+      <Faq />
     </>
   );
 }
