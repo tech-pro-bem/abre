7c82d859485c8e5d2b3e2180810fd5440e64245c
Pedro Menezes <pdrmenezes1@gmail.com>
2024-11-18 19:17:19 -0300

    chore: retry deploy

--- src/mock-data/acolhimento.tsx
+++ src/mock-data/acolhimento.tsx
@@ -4,7 +4,7 @@ export const mock_acolhimento_data = [
     description:
       "Confira uma lista de instituições que discutem esquizofrenia e outros temas ligados ao transtorno. Ao escolher uma instituição, iremos te redirecionar para uma página externa para você pesquisar mais sobre o assunto.",
     button_text: "Conferir instituições",
-    button_href: "",
+    button_href: "/",
   },
   {
     title: "Publicações do IPUB",
@@ -18,6 +18,6 @@ export const mock_acolhimento_data = [
     description:
       "Centros de Atenção Psicossocial (CAPS) são lugares onde a comnunidade consegue atendimento focado em saúde mental, seja para situações difíceis ou reabilitação psicossocial.",
     button_text: "Conferir CAPS",
-    button_href: "",
+    button_href: "/",
   },
 ];
