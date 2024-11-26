62be38d9a5d4abe6a5d012f97bd1745cec6ad5a1
Kalini Veloso <kaliniveloso41@gmail.com>
2024-11-26 12:52:06 -0300

    refactor: inserting mocked data

--- src/components/footer/footer.tsx
+++ src/components/footer/footer.tsx
@@ -2,7 +2,32 @@ import Image from "next/image";
 import styles from "./styles.module.css";
 import { NavBar } from "./navbar/navbar";
 import { FacebookIcon, InstagramIcon, YoutubeIcon } from "../icons";
-
+export const footer_text = {
+  title_donation: "Faça uma doação",
+  text_donation: `Basta escanear o QR code.
+  Banco Itaú
+  Ag: 0845-1. CC: 21860-1
+  Pix: 05.469.302/0001-27`,
+  text_lgpd: ` A ABRE valoriza sua privacidade e segue as diretrizes da Lei Geral de Proteção de
+  Dados Pessoais (LGPD) para proteger suas informações. Também utilizamos cookies para
+  melhorar sua navegação em nosso site.`,
+};
+const wordsInBold = [" Banco Itaú", "Ag:", "CC:", "Pix:"];
+const stylizeWords = (text: string) => {
+  const regex = new RegExp(`(${wordsInBold.join("|")})`, "gi");
+  return text.split(regex).map((words, index) =>
+    wordsInBold.includes(words) ? (
+      <span
+        key={index}
+        className={styles.donation_text_strong}
+      >
+        {words}
+      </span>
+    ) : (
+      words
+    )
+  );
+};
 export const Footer = () => {
   return (
     <section className={styles.footer}>
@@ -69,27 +94,22 @@ export const Footer = () => {
               />
             </div>
             <div className={styles.donation_title_text}>
-              <h2 className={styles.donation_title}>Faça uma doação</h2>
+              <h2 className={styles.donation_title}>{footer_text.title_donation}</h2>
               <div className={styles.donation_text}>
-                <p>Basta escanear o QR code.</p>
-                <strong className={styles.donation_text_strong}> Banco Itaú</strong>
-                <p>
-                  <strong className={styles.donation_text_strong}>Ag:</strong> 0845-1.
-                  <strong className={styles.donation_text_strong}> CC:</strong> 21860-1
-                </p>
-                <p>
-                  <strong className={styles.donation_text_strong}>Pix:</strong> 05.469.302/0001-27
-                </p>
+                {footer_text.text_donation.split("\n").map((paragraph, index) => (
+                  <p
+                    className={styles.donation_text}
+                    key={index}
+                  >
+                    {stylizeWords(paragraph)}
+                  </p>
+                ))}
               </div>
             </div>
           </div>
           <div className={styles.footer_content_container}>
             <NavBar className={styles.content_navbar} />
-            <p className={styles.content_text}>
-              A ABRE valoriza sua privacidade e segue as diretrizes da Lei Geral de Proteção de
-              Dados Pessoais (LGPD) para proteger suas informações. Também utilizamos cookies para
-              melhorar sua navegação em nosso site.
-            </p>
+            <p className={styles.content_text}>{footer_text.text_lgpd}</p>
           </div>
         </div>
         <ul className={styles.footer_icons_mobile}>
