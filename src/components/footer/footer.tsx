be517db18ee85c79d85308e292bebaf037e251cf
Kalini Veloso <kaliniveloso41@gmail.com>
2024-12-12 15:05:46 -0300

    refactor: modifying structure and style and creating new breakpoints

--- src/components/footer/footer.tsx
+++ src/components/footer/footer.tsx
@@ -2,6 +2,7 @@ import Image from "next/image";
 import styles from "./styles.module.css";
 import { NavBar } from "./navbar/navbar";
 import { FacebookIcon, InstagramIcon, YoutubeIcon } from "../icons";
+import { FooterSocials } from "./socials";
 export const footer_text = {
   title_donation: "Faça uma doação",
   text_donation: `Basta escanear o QR code.
@@ -31,131 +32,45 @@ export const Footer = () => {
   return (
     <footer className={styles.footer}>
       <div className={styles.footer_container}>
-        <div className={styles.footer_logo_icons_desktop}>
+        <div className={styles.logo_icons_container}>
           <Image
             className={styles.logo_abre}
             src="/logo/logo.svg"
-            width={500}
-            height={500}
+            width={180}
+            height={45}
             alt="Logo - Abre"
           />
-          <ul className={styles.icons_desktop}>
-            <li>
-              <a
-                href="https://www.facebook.com/abre.esquizofrenia/"
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                <FacebookIcon
-                  className={styles.icon}
-                  width={24}
-                  height={24}
-                />
-              </a>
-            </li>
-            <li>
-              <a
-                href="https://www.instagram.com/abreesquizofrenia/"
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                <InstagramIcon
-                  className={styles.icon}
-                  width={23}
-                  height={24}
-                />
-              </a>
-            </li>
-            <li>
-              <a
-                href="https://www.youtube.com/@abrebrasil2014"
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                <YoutubeIcon
-                  className={styles.icon}
-                  width={26}
-                  height={26}
-                />
-              </a>
-            </li>
-          </ul>
+          <FooterSocials className={styles.icons_container_desktop} />
         </div>
-        <Image
-          className={styles.logo_abre_mobile}
-          src="/logo/logo.svg"
-          width={500}
-          height={500}
-          alt="Logo - Abre"
-        />
-        <div className={styles.footer_donation_container}>
-          <div className={styles.donation_qr_code_container}>
+        <div className={styles.footer_content}>
+          <div className={styles.donation_container}>
             <Image
               className={styles.donation_qr_code}
               src="/img_qr_code.svg"
               alt="qrcode - Abre"
-              fill
+              width={123}
+              height={123}
             />
-          </div>
-          <div className={styles.donation_title_text}>
-            <h2 className={styles.donation_title}>{footer_text.title_donation}</h2>
-            <div className={styles.donation_text}>
-              {footer_text.text_donation.split("\n").map((paragraph, index) => (
-                <p
-                  className={styles.donation_paragraph}
-                  key={index}
-                >
-                  {stylizeWords(paragraph)}
-                </p>
-              ))}
+            <div className={styles.donation_title_text}>
+              <h2 className={styles.donation_title}>{footer_text.title_donation}</h2>
+              <div className={styles.donation_text}>
+                {footer_text.text_donation.split("\n").map((paragraph, index) => (
+                  <p
+                    className={styles.donation_paragraph}
+                    key={index}
+                  >
+                    {stylizeWords(paragraph)}
+                  </p>
+                ))}
+              </div>
             </div>
           </div>
-        </div>
-        <div className={styles.content_navbar}>
           <NavBar />
+          <div className={styles.lgpd_icons_container}>
+            <p className={styles.content_text}>{footer_text.text_lgpd}</p>
+            <FooterSocials className={styles.icons_container_mobile} />
+          </div>
         </div>
-        <p className={styles.content_text}>{footer_text.text_lgpd}</p>
-        <ul className={styles.icons_mobile}>
-          <li>
-            <a
-              href="https://www.facebook.com/abre.esquizofrenia/"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              <FacebookIcon
-                className={styles.icon}
-                width={24}
-                height={24}
-              />
-            </a>
-          </li>
-          <li>
-            <a
-              href="https://www.instagram.com/abreesquizofrenia/"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              <InstagramIcon
-                className={styles.icon}
-                width={23}
-                height={24}
-              />
-            </a>
-          </li>
-          <li>
-            <a
-              href="https://www.youtube.com/@abrebrasil2014"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              <YoutubeIcon
-                className={styles.icon}
-                width={26}
-                height={26}
-              />
-            </a>
-          </li>
-        </ul>
       </div>
     </footer>
   );
