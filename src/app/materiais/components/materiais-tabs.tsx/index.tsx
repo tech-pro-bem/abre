1a3bbe17d7b5b0986aca26d7dec53b4b91dddfe6
Pedro Menezes <pdrmenezes1@gmail.com>
2024-12-15 18:57:45 -0300

    feat: creat tabs component

---
+++ src/app/materiais/components/materiais-tabs.tsx/index.tsx
@@ -0,0 +1,24 @@
+"use client";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+import styles from "./styles.module.css";
+
+const TABS = [
+  { title: "Livros", slug: "livros" },
+  { title: "Relatórios", slug: "relatorios" },
+  { title: "Galeria", slug: "galeria" },
+];
+
+export function MateriaisTabs() {
+  const pathName = usePathname();
+  const isActiveTab = (slug: string) => pathName.includes(slug);
+  return (
+    <ul className={styles.tabs_container}>
+      {TABS.map(({ title, slug }) => (
+        <li className={`${styles.tab} ${isActiveTab(slug) ? styles.active : ""}`}>
+          <Link href={`${slug}`}>{title}</Link>
+        </li>
+      ))}
+    </ul>
+  );
+}
