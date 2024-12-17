02931caffa74edea0b06a9492c35ded5e0b51bb3
Pedro Menezes <pdrmenezes1@gmail.com>
2024-12-17 18:24:08 -0300

    refactor: replace axios with std fetch api

--- src/lib/contentful.ts
+++ src/lib/contentful.ts
@@ -1,31 +1,28 @@
 import { CONTENT_TYPES, GetContentByContentTypeResponse } from "@/types/contentful.types";
-import axios from "axios";
 
 const SPACE_ID = process.env.CONTENTFUL_SPACE_ID;
 const ENVIRONMENT = process.env.CONTENTFUL_ENVIRONMENT;
 const TOKEN = process.env.CONTENTFUL_ACCESS_TOKEN;
+const BASE_URL = "https://cdn.contentful.com";
 
-export const contentful = axios.create({
-  baseURL: "https://cdn.contentful.com/",
-  headers: { Authorization: `Bearer ${TOKEN}` },
-});
-
-export async function getContentByContentType<T>(contentType: CONTENT_TYPES | string) {
+export async function getContentByContentType<T>(
+  contentType: CONTENT_TYPES | string
+): Promise<GetContentByContentTypeResponse<T>> {
   if (!contentType) throw new Error("Missing Content Type");
 
-  try {
-    const { data, status } = await contentful.get<GetContentByContentTypeResponse<T>>(
-      `spaces/${SPACE_ID}/environments/${ENVIRONMENT}/entries/?select=fields&content_type=${contentType}`
-    );
-
-    if (status !== 200) {
-      console.error("Unexpected response status:", status);
-      return null;
+  const response = await fetch(
+    `${BASE_URL}/spaces/${SPACE_ID}/environments/${ENVIRONMENT}/entries/?select=fields&content_type=${contentType}`,
+    {
+      headers: {
+        Authorization: `Bearer ${TOKEN}`,
+        "Content-Type": "application/json",
+      },
     }
+  );
 
-    console.log("getContentByContentType Data:", data);
-    return data;
-  } catch (error) {
-    console.error("Error fetching contentful data:", error);
+  if (!response.ok) {
+    throw new Error(`Erro ao buscar conteúdo: ${response.status}, ${response.statusText}`);
   }
+
+  return response.json();
 }
