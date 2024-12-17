e05a46692b518cc907503d26858398c8d41a4cf5
Kalini Veloso <kaliniveloso41@gmail.com>
2024-12-17 14:30:27 -0300

    feat: creation of the gallery section

---
+++ src/app/materials/page.tsx
@@ -0,0 +1,8 @@
+import { Album } from "../../components/albums/index";
+export default function Material() {
+  return (
+    <>
+      <Album />
+    </>
+  );
+}
--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -2,25 +2,39 @@ import Image from "next/image";
 import styles from "./styles.module.css";
 import { Navbar } from "./navbar";
 import { SocialMedias } from "../social-medias";
+import Link from "next/link";
 
 export const Header = () => {
   return (
     <header className={styles.header}>
       <div className={styles.header__logo_container}>
-        <figure className={styles.header__logo}>
+        <Link
+          href={"/"}
+          className={styles.header__logo}
+        >
           <Image
             src="/logo/logo.svg"
             alt="logo"
             width={148}
             height={34}
           />
-        </figure>
-        <button className={styles.header__button}>Acessar materiais</button>
+        </Link>
+        <Link
+          href="/materials"
+          className={styles.header__button}
+        >
+          Acessar materiais
+        </Link>
       </div>
 
       <div className={styles.header__nav_container}>
         <Navbar />
-        <button className={styles.header__button}>Acessar materiais</button>
+        <Link
+          href="/materials"
+          className={styles.header__button}
+        >
+          Acessar materiais
+        </Link>
         <SocialMedias />
       </div>
     </header>
