92455571c95ff06d32b91842af8e59ccf98dbf9a
JaySant <jaysant15@gmail.com>
2025-01-15 19:01:08 -0300

    refactor: updated naming, removed mock, and integrated componentized action icons

--- src/app/materiais/livros/page.tsx
+++ src/app/materiais/livros/page.tsx
@@ -1,88 +1,54 @@
 import styles from "./styles.module.css";
-import { DownloadIcon, OpenNewArrowIcon } from "@/components/icons";
-import { formatFileName } from "@/utils/format-file-name";
 import Image from "next/image";
+import { mock_livros_datas } from "@/mock-data/books";
+import DownloadLink from "../components/icon-links/download-link";
+import OpenLink from "../components/icon-links/open-link";
 
-const mock_livros_datas = [
-  {
-    image: "/books/conversando_sobre_a_esquizofrenia_1.webp",
-    title: "Conversando sobre a esquizofrenia 1",
-    subtitle: "Entenda como ela acontece",
-  },
-  {
-    image: "/books/conversando_sobre_a_esquizofrenia_2.webp",
-    title: "Conversando sobre a esquizofrenia 2",
-    subtitle: "Caminho até o diagnóstico",
-  },
-  {
-    image: "/books/conversando_sobre_a_esquizofrenia_3.webp",
-    title: "Conversando sobre a esquizofrenia 3",
-    subtitle: "A importância do tratamento",
-  },
-  {
-    image: "/books/conversando_sobre_a_esquizofrenia_4.webp",
-    title: "Conversando sobre a esquizofrenia 4",
-    subtitle: "Estigma - Como as pessoas se sentem?",
-  },
-  {
-    image: "/books/conversando_sobre_a_esquizofrenia_5.webp",
-    title: "Conversando sobre a esquizofrenia 5",
-    subtitle: "O convívio familiar",
-  },
-  {
-    image: "/books/conversando_sobre_a_esquizofrenia_6.webp",
-    title: "Conversando sobre a esquizofrenia 6",
-    subtitle: "Recuperação e novas perspectivas",
-  },
-  {
-    image: "/books/guia_prático_sobre_psicoses.webp",
-    title: "Guia prático sobre psicoses para profissionais da Atenção Básica",
-    subtitle: "",
-  },
-];
+interface Books {
+  image: string;
+  title: string;
+  subtitle?: string;
+  url: string;
+}
 
 export default function LivrosPage() {
   return (
-    <section className={styles.books_section}>
-      <div className={styles.books_container}>
-        {mock_livros_datas.map(({ image, title, subtitle }) => (
-          <div
-            key={title}
-            className={styles.book}
-          >
-            <Image
-              src={image}
-              alt={title}
-              width={148}
-              height={222}
-              className={styles.book_image}
-            />
-            <div className={styles.book_info}>
-              <h3>{title}</h3>
-              <p>{subtitle}</p>
-              <div className={styles.download}>
-                <p>Baixar o arquivo</p>
-                <a
-                  href="https://abre.org/materiais/"
-                  download={`${formatFileName(title)}.pdf`}
-                >
-                  <DownloadIcon />
-                </a>
+    <section className={styles.section}>
+      {mock_livros_datas.map(({ image, title, subtitle, url }: Books) => (
+        <article
+          key={title}
+          className={styles.books_container}
+        >
+          <Image
+            src={image}
+            alt={title}
+            width={148}
+            height={222}
+            className={styles.image}
+          />
+          <div className={styles.books_content}>
+            <div className={styles.title_container}>
+              <h3 className={styles.title}>{title}</h3>
+              <p className={styles.subtitle}>{subtitle}</p>
+            </div>
+            <div className={styles.container_buttons}>
+              <div className={styles.download_link}>
+                <DownloadLink
+                  title={title}
+                  url={url}
+                  label="Baixar o arquivo"
+                />
               </div>
-              <div className={styles.open}>
-                <p>Abrir no navegador</p>
-                <a
-                  href="https://abre.org/materiais/"
-                  target="_blank"
-                  rel="noopener noreferrer"
-                >
-                  <OpenNewArrowIcon />
-                </a>
+              <div className={styles.open_link}>
+                <OpenLink
+                  url={url}
+                  label="Abrir no navegador"
+                />
               </div>
             </div>
           </div>
-        ))}
-      </div>
+        </article>
+      ))}
     </section>
   );
 }
