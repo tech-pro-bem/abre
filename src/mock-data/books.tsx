474f3ec64e549609a2e1ac943a4adedf13e39ed2
JaySant <jaysant15@gmail.com>
2025-01-15 18:48:45 -0300

    refactor: reorganized mock and added the 'url' property

---
+++ src/mock-data/books.tsx
@@ -0,0 +1,44 @@
+export const mock_livros_datas = [
+  {
+    image: "/books/conversando_sobre_a_esquizofrenia_1.webp",
+    title: "Conversando sobre a esquizofrenia 1",
+    subtitle: "Entenda como ela acontece",
+    url: "https://abre.org/materiais/",
+  },
+  {
+    image: "/books/conversando_sobre_a_esquizofrenia_2.webp",
+    title: "Conversando sobre a esquizofrenia 2",
+    subtitle: "Caminho até o diagnóstico",
+    url: "https://abre.org/materiais/",
+  },
+  {
+    image: "/books/conversando_sobre_a_esquizofrenia_3.webp",
+    title: "Conversando sobre a esquizofrenia 3",
+    subtitle: "A importância do tratamento",
+    url: "https://abre.org/materiais/",
+  },
+  {
+    image: "/books/conversando_sobre_a_esquizofrenia_4.webp",
+    title: "Conversando sobre a esquizofrenia 4",
+    subtitle: "Estigma - Como as pessoas se sentem?",
+    url: "https://abre.org/materiais/",
+  },
+  {
+    image: "/books/conversando_sobre_a_esquizofrenia_5.webp",
+    title: "Conversando sobre a esquizofrenia 5",
+    subtitle: "O convívio familiar",
+    url: "https://abre.org/materiais/",
+  },
+  {
+    image: "/books/conversando_sobre_a_esquizofrenia_6.webp",
+    title: "Conversando sobre a esquizofrenia 6",
+    subtitle: "Recuperação e novas perspectivas",
+    url: "https://abre.org/materiais/",
+  },
+  {
+    image: "/books/guia_prático_sobre_psicoses.webp",
+    title: "Guia prático sobre psicoses para profissionais da Atenção Básica",
+    subtitle: "",
+    url: "https://abre.org/materiais/",
+  },
+];
