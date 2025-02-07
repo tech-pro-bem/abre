ee30679fec13ace256f5adc90952e67d107509a4
JaySant <jaysant15@gmail.com>
2025-02-07 17:58:23 -0300

    fix: update URL path

--- src/app/materiais/galeria/page.tsx
+++ src/app/materiais/galeria/page.tsx
@@ -7,6 +7,7 @@ import { useState } from "react";
 import Pagination from "@/components/pagination";
 import FilterButton from "@/components/filter-button";
 import { Albums } from "@/mock-data/galeria";
+import { generateSlug } from "@/utils/generate-slug";
 
 export default function GaleriaPage() {
   const [currentAlbums, setCurrentAlbums] = useState<typeof Albums>([]);
@@ -20,7 +21,7 @@ export default function GaleriaPage() {
           <div key={index}>
             <Link
               className={styles.album_items}
-              href="/"
+              href={`/materiais/galeria/${generateSlug(album.title)}`}
             >
               <div className={styles.image_quantity}>
                 <Image
