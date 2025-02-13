77138bea4e7ad5708b01a5262cb4fc332f2e24fb
Pedro Menezes <pdrmenezes1@gmail.com>
2025-02-12 21:02:47 -0300

    feat: feat image remotePattern protocol

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    remotePatterns: [{ hostname: "images.ctfassets.net" }],
+    remotePatterns: [{ hostname: "images.ctfassets.net", protocol: "https" }],
   },
 };
 
