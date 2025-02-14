a0f9a7055e1ce3af43ab573a2e0487f2d2ed31d6
Kalini Veloso <kaliniveloso41@gmail.com>
2025-02-14 12:24:46 -0300

    refactor: keeping only the animation

--- src/app/loading.tsx
+++ src/app/loading.tsx
@@ -2,11 +2,8 @@ import styles from "./loading.module.css";
 
 export default function Loading() {
   return (
-    <section className={styles.container}>
-      <h1 className={styles.title}>Carregando, por favor aguarde...</h1>
-      <div className={styles.container_loading}>
-        <div className={styles.loading}></div>
-      </div>
+    <section className={styles.container_loading}>
+      <div className={styles.loading}></div>
     </section>
   );
 }
